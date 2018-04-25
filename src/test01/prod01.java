ab6df653f6f36a2f02a784fdae462dea813e4f4e
akash <pratap_the_knight@yahoo.com>
2018-04-25 00:53:22 -0400

    code changes

---
+++ src/test01/prod01.java
@@ -0,0 +1,60 @@
+package test01;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class prod01 {
+	
+	static String driverPath = "path to chrome driver";
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		System.out.println("*******************");
+		System.out.println("launching chrome browser");
+		//System.setProperty("webdriver.chrome.driver", driverPath+"chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testGooglePageTitleInIEBrowser() throws Exception {
+		driver.navigate().to("http://www.google.com");
+		String strPageTitle = driver.getTitle();
+		System.out.println("Page title: - "+strPageTitle);
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+		//prod01.takeSnapShot(driver, "C:\\test\\screenshots\\screenshot.png");
+		prod01.takeSnapShot(driver, System.getProperty("user.dir")+"\\screenshot\\screenshot.png");
+	}
+	
+    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+        //Call getScreenshotAs method to create image file
+                File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+            //Move image file to new destination
+                File DestFile=new File(fileWithPath);
+                //Copy file at destination
+                FileUtils.copyFile(SrcFile, DestFile);
+
+    }	
+
+	@AfterClass
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing chrome browser");
+			driver.quit();
+		}
+	}
+	
+}
